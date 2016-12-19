9a75b6222e8a02f933b87f81d20dbea492ad4190
arshadsbwd <arshads.bwd@gmail.com>
2016-12-19 14:52:57 +0530

    Initial commit

---
+++ sample-chat/src/main/java/com/quickblox/sample/chat/ui/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.quickblox.sample.chat.ui.activity;
+
+import android.os.Bundle;
+
+import com.quickblox.sample.chat.R;
+import com.quickblox.sample.chat.utils.SharedPreferencesUtil;
+import com.quickblox.sample.core.ui.activity.CoreSplashActivity;
+
+public class SplashActivity extends CoreSplashActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        proceedToTheNextActivityWithDelay();
+    }
+
+    @Override
+    protected String getAppName() {
+        return getString(R.string.splash_app_title);
+    }
+
+    @Override
+    protected void proceedToTheNextActivity() {
+        if (SharedPreferencesUtil.hasQbUser()) {
+            DialogsActivity.start(this);
+        } else {
+            LoginActivity.start(this);
+        }
+        finish();
+    }
+}
---
+++ sample-users/src/main/java/com/quickblox/sample/user/activities/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.quickblox.sample.user.activities;
+
+import android.os.Bundle;
+
+import com.quickblox.sample.core.ui.activity.CoreSplashActivity;
+import com.quickblox.sample.user.R;
+
+public class SplashActivity extends CoreSplashActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        proceedToTheNextActivityWithDelay();
+    }
+
+    @Override
+    protected String getAppName() {
+        return getString(R.string.splash_app_title);
+    }
+
+    @Override
+    protected void proceedToTheNextActivity() {
+        UsersListActivity.start(this);
+        finish();
+    }
+}
